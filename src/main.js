3edbd6293de72aaa6d5f5592e84f19972528d1b0
Olha Hizhytska <olgainfotech@gmail.com>
2021-02-08 16:44:04 -0800

    Add 2 other buttons
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function(){
+
+
   $("#findgiphy").click(function(){
     const search = $("#giphy").val();
     $("#findgiphy").val("");
+    
+    // function clearFields(response) {
+    //   for (let i =0; i < response.data.length; i ++){
+    //   $("#img"+i).remove;
+    //   }
+    // }
 
     let request = new XMLHttpRequest();
     const apiURL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=5&rating=pg-13`;
@@ -16,6 +24,7 @@ $(document).ready(function(){
       if (this.readyState === 4 && this.status === 200){
         const response = JSON.parse(this.responseText);
         getElements(response);
+        
       }
     }
 
@@ -24,8 +33,57 @@ $(document).ready(function(){
 
     function getElements(response){
       for (let index = 0; index < response.data.length; index ++){
-        $("#results0").append(`<img src='${response.data[index].images.original.url}' alt="result gif">`);
+        $("#results0").append(`<li><img src='${response.data[index].images.original.url}' alt="result gif"></li>`);
+        
       }
+      // clearFields(response);
     }
   });
+
+  $('#findtrendgiphy').click(function() {
+    
+    let request = new XMLHttpRequest();
+
+    const api = `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=pg-13`;
+
+    request.onreadystatechange = function(){
+      //console.log(this.readyState)
+      if (this.readyState === 4 && this.status === 200){
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+        
+      }
+    }
+
+    request.open("GET", api, true);
+    request.send();
+
+
+    function getElements(response) {
+      for (let index = 0; index < response.data.length; index ++) {
+        $('#result').append(`<li><img src='${response.data[index].images.original.url}' alt="result gif"></li>`);
+      } 
+    }
+  });
+
+  $('#randomgiphy').click(function() {
+    let request = new XMLHttpRequest();
+
+    const randomURL = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&rating=pg-13`
+
+    request.onreadystatechange = function(){
+     console.log(this.readyState)
+      if (this.readyState === 4 && this.status === 200){
+        const response = JSON.parse(this.responseText);          getElements(response);
+          
+      }
+    }
+
+    request.open("GET", randomURL, true);
+    request.send();
+
+    function getElements(response) {
+      $('#randomresult').html(`<img src='${response.data.url}' alt="result gif">`);
+    }
+  })
 });
